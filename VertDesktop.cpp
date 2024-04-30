588d5e120c1cad18d6a1c438f5ccc33a8e5bbf8d
Andrew <kezin636@gmail.com>
2024-04-30 22:07:28 +0300

    Add project files.

---
+++ VertDesktop.cpp
@@ -0,0 +1,12 @@
+﻿// VertDesktop.cpp : Defines the entry point for the application.
+//
+
+#include "VertDesktop.h"
+
+using namespace std;
+
+int main()
+{
+	cout << "Hello CMake." << endl;
+	return 0;
+}
